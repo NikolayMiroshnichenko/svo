14f3f9ac8102b08d8415c8e664359123b39db8a8
NikolayMiroshnichenko <36457433+NikolayMiroshnichenko@users.noreply.github.com>
2020-04-01 18:52:31 +0300

    15

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const input =  document.querySelector("#phone"); 
+
+window.intlTelInput (input, {
+    utilsScript: 'js/utils.js',
+    defaultCountry: 'auto',
+    separateDialCode: true,
+    allowDropdown: false,
+    customPlaceholder: function(selectedCountryPlaceholder, selectedCountryData) {
+        return "";
+    },
+    nationalMode: false,
+    initialCountry: 'auto',
+    geoIpLookup: function(callback) {
+        $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            callback(countryCode);
+        });
+    },
+    preferredCountries: ['ua', 'ru', 'by', 'kz']
+
+});
+
+input.addEventListener('focus', codeCauntry)
+
+function codeCauntry(e) {
+    const countryData = document.querySelector('.iti__selected-dial-code').textContent;
+
+    if(input.value !== "") return;
+    input.value = countryData;
+}
+
+
+
+
